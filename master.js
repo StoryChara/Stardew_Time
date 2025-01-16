663622fcc9b61fc9db5f5f57a733eff0a100e648
StoryChara <mjarah@unal.edu.co>
2025-01-16 12:35:38 -0500

    Master

--- master.js
+++ master.js
@@ -1,5 +1,6 @@
 const buttons = [
   { name: "Home", href: "index.html" },
+  { name: "Master", href: "Master.html"},
   { name: "Cultivo", href: "cultivo.html" },
   { name: "Pesca", href: "pesca.html"}
 ];
